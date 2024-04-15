222d57b29344914bdc1e18315cb75a4b6feee3fd
Leonardo <leonardo.taglienti@gmail.com>
2024-04-15 12:32:25 +0200

    Updated header (links to be updated) and logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -85,11 +85,17 @@ function MobileNavigation() {
             as="div"
             className="absolute inset-x-0 top-full mt-4 flex origin-top flex-col rounded-2xl bg-white p-4 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5"
           >
-            <MobileNavLink href="#features">Features</MobileNavLink>
-            <MobileNavLink href="#testimonials">Testimonials</MobileNavLink>
-            <MobileNavLink href="#pricing">Pricing</MobileNavLink>
+            <MobileNavLink href="#features">Das Spiel</MobileNavLink>
+            <MobileNavLink href="#testimonials">
+              Dein Trainingsbereich
+            </MobileNavLink>
+            <MobileNavLink href="#pricing">Vorteile</MobileNavLink>
+            <MobileNavLink href="#pricing">Feedback</MobileNavLink>
+            <MobileNavLink href="#pricing">FAQ</MobileNavLink>
             <hr className="m-2 border-slate-300/40" />
-            <MobileNavLink href="/login">Sign in</MobileNavLink>
+            <Button href="/register" color="yellow">
+              Jetzt spielen
+            </Button>
           </Popover.Panel>
         </Transition.Child>
       </Transition.Root>
@@ -102,26 +108,32 @@ export function Header() {
     <header className="py-10">
       <Container>
         <nav className="relative z-50 flex justify-between">
-          <div className="flex items-center md:gap-x-12">
+          <div className="flex items-center lg:gap-x-12">
             <Link href="#" aria-label="Home">
               <Logo className="h-10 w-auto" />
             </Link>
-            <div className="hidden md:flex md:gap-x-6">
+            {/* <div className="hidden md:flex md:gap-x-6">
               <NavLink href="#features">Features</NavLink>
               <NavLink href="#testimonials">Testimonials</NavLink>
               <NavLink href="#pricing">Pricing</NavLink>
-            </div>
+            </div> */}
           </div>
           <div className="flex items-center gap-x-5 md:gap-x-8">
-            <div className="hidden md:block">
-              <NavLink href="/login">Sign in</NavLink>
+            <div className="hidden lg:flex lg:gap-x-6">
+              <NavLink href="#features">Das Spiel</NavLink>
+              <NavLink href="#testimonials">Dein Trainingsbereich</NavLink>
+              <NavLink href="#pricing">Vorteile</NavLink>
+              <NavLink href="#pricing">Feedback</NavLink>
+              <NavLink href="#pricing">FAQ</NavLink>
             </div>
-            <Button href="/register" color="yellow">
-              <span>
-                Get started <span className="hidden lg:inline">today</span>
-              </span>
+            <Button
+              href="/register"
+              className="hidden lg:inline"
+              color="yellow"
+            >
+              Jetzt spielen
             </Button>
-            <div className="-mr-1 md:hidden">
+            <div className="-mr-1 lg:hidden">
               <MobileNavigation />
             </div>
           </div>
